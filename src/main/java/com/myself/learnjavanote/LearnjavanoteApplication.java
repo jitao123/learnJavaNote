e2f10b0c4969fd18fa790acd6c7eed199988fc2b
@jitao123 <1432599195@qq.com>
2021-02-20 11:13:08 +0800

    初始化项目

---
+++ src/main/java/com/myself/learnjavanote/LearnjavanoteApplication.java
@@ -0,0 +1,13 @@
+package com.myself.learnjavanote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnjavanoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnjavanoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myself/learnjavanote/LearnjavanoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myself.learnjavanote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnjavanoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
