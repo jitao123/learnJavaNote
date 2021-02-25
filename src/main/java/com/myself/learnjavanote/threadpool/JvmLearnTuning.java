032fd809f2336cf583dd82dd475f67491c620ecf
@jitao123 <1432599195@qq.com>
2021-02-25 17:23:10 +0800

    jvm研究

--- src/main/java/com/myself/learnjavanote/threadpool/JvmLearnTuning.java
+++ src/main/java/com/myself/learnjavanote/threadpool/JvmLearnTuning.java
@@ -11,6 +11,14 @@ import java.util.List;
 public class JvmLearnTuning {
     private byte[] bytes = new byte[1024 * 200];
 
+    /**
+     * 1个方法就是栈帧，这里创建的是无数个方法，
+     * 那么就会一直占用栈内存，导致栈溢出
+     */
+    public void foo(){
+        foo();
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("hello～～～～");
         List<JvmLearnTuning> list = new ArrayList<>();
